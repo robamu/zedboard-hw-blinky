85ae666e24ee842a70dc6fca0f21c9b642a4ef89
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-01-06 20:30:30 +0100

    update default proj info

--- def_project_info.tcl
+++ def_project_info.tcl
@@ -2,11 +2,15 @@
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
     set_property "part" "xc7z020clg484-1" $project_obj
-    set_property BOARD_PART_REPO_PATHS "${::env(HOME)}/.Xilinx/Vivado/2021.2/xhub/board_store/xilinx_board_store" $project_obj
+    set_property BOARD_PART_REPO_PATHS "${::env(HOME)}/.Xilinx/Vivado/2021.1/xhub/board_store/xilinx_board_store" $project_obj
     set_property "board_part" "avnet.com:zedboard:part0:1.4" $project_obj
     set_property "default_lib" "xil_defaultlib" $project_obj
     set_property "simulator_language" "Mixed" $project_obj
     set_property "target_language" "Verilog" $project_obj
+
+    set repo_path [file normalize [file dirname [info script]]/..]
+    add_files -quiet -norecurse $repo_path/src/hdl/vhdl
+    add_files -quiet -norecurse $repo_path/src/hdl/verilog
 }
 
 proc set_project_properties_pre_add_repo {proj_name} {
